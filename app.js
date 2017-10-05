09a6f43efc9a0e4efff851a48dce6175c767ef5e
Nick Gibson <Nicholas.a.Gibson@Gmail.com>
2017-10-05 00:32:04 -0700

    going to bed

--- app.js
+++ app.js
@@ -87,24 +87,58 @@ function clickChoice(e) {
   for(var i = 0; i < Product.allProducts.length; i++) {
     if(e.target.alt === Product.allProducts[i].alt) {
       Product.allProducts[i].clickCounter += 1;
-      console.log('woo');
     }
   }
   console.log(Product.allProducts);
+  if(Product.totalClicks > 25) {
+    Product.allProducts.removeEventListener('click', handleClick);
+    return showResults();
+  }
   randomProduct();
   renderProduct();
-  if(Product.totalClicks >= 25) {
-    Product.allProducts.removeEventListener('click', handleClick);
-    // display the results
-    showResults();
-    //add localStore. here for 'every 25'
-  }
 };
 
 function showResults(){
-
+  document.getElementById('images').innerHTML = '';
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: Product.allProducts.clickCounter,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
 }
 
+Product.render.addEventListener('click', clickChoice);
 randomProduct();
 renderProduct();
-Product.render.addEventListener('click', clickChoice);
